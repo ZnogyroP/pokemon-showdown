774a483579052acc9c9b16d0a7b178606106f3da
DuoM2 <duoothenub@gmail.com>
2020-10-03 18:20:58 -0400

    Update items.ts

--- data/mods/duomod/items.ts
+++ data/mods/duomod/items.ts
@@ -59,41 +59,5 @@
 		gen: 2,
 		desc: "If the holder is Fire-type and gets hit by a Fire-type move, the holder's Fire-type attacks have 1.5x power.",
 	},
-	waterbucket: {
-		name: "Water Bucket",
-		spritenum: 430,
-		fling: {
-			basePower: 10,
-		},
-		onSourceModifyDamage(damage, source, target, move) {
-			if pokemon.hasType('Water') {
-			if (target.getMoveHitData(move).typeMod > 0) {
-				this.debug('Water Bucket boost');
-				return this.chainModify(2);
-			}
-			else if (target.getMoveHitData(move).typeMod < 0) {
-				this.debug('Water Bucket debuff');
-				return this.chainModify(0.5);
-			}}
-		},
-		num: 1503.1,
-		gen: 3,
-		desc: "If the holder is Water-type, they take half damage from resisted moves and double damage from super-effective moves.",
-	},
-	lightfeather: {
-		name: "Light Feather",
-		spritenum: 535,
-		basePowerCallback(pokemon, target, move) {
-			if pokemon.hasType('Flying') {
-			if (this.queue.willMove(target)) {
-				this.debug('Light Feather damage boost');
-				return move.basePower * 1.2;
-			}
-			this.debug('Light Feather NOT boosted');
-			return move.basePower;
-		}},
-		num: 1504.1,
-		gen: 3,
-		desc: "Boosts the power of a Flying-type holder by 1.2x if they move first.",
-	},
+
 };
