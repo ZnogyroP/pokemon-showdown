46d2ff30b0fa22274d9a586b31ba09c0cfc40246
DuoM2 <duoothenub@gmail.com>
2020-10-03 12:30:03 -0400

    Update moves.ts

--- data/mods/duomod/moves.ts
+++ data/mods/duomod/moves.ts
@@ -21989,7 +21989,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1, contact: 1},
 		onModifyType(move, pokemon) {
-			let type = pokemon.types[1];
+			let type = newType;
 			if (type === "Bird") type = "???";
 			move.type = type;
 		},
