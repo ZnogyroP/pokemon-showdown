b4e3db914908407698ee674f201425de5dd2287e
DuoM2 <duoothenub@gmail.com>
2020-10-03 21:01:41 -0400

    Update items.ts

--- data/mods/duomod/items.ts
+++ data/mods/duomod/items.ts
@@ -1,96 +1,48 @@
 ﻿export const Items: {[itemid: string]: ItemData} = {
-	waterbucket: {
-		name: "Water Bucket",
-		spritenum: 430,
+	drytorch: {
+		name: "Dry Torch",
+		spritenum: 61,
 		fling: {
-			basePower: 10,
+			basePower: 30,
 		},
-		onSourceModifyDamage(damage, source, target, move) {
-			if pokemon.hasType('Water') {
-			if (target.getMoveHitData(move).typeMod > 0) {
-				this.debug('Water Bucket boost');
-				return this.chainModify(2);
-			}
-			else if (target.getMoveHitData(move).typeMod < 0) {
-				this.debug('Water Bucket debuff');
-				return this.chainModify(0.5);
+		onTryHit(target, source, move) {
+			if (pokemon.hasType('Fire')) {
+			if (target !== source && move.type === 'Fire') {
+				move.accuracy = true;
+				if (!target.addVolatile('drytorch')) {
+				}
+				return null;
 			}}
 		},
-		num: 1503.1,
-		gen: 3,
-		desc: "If the holder is Water-type, they take half damage from resisted moves and double damage from super-effective moves.",
-	},
-	lightfeather: {
-		name: "Light Feather",
-		spritenum: 535,
-		basePowerCallback(pokemon, target, move) {
-			if pokemon.hasType('Flying') {
-			if (this.queue.willMove(target)) {
-				this.debug('Light Feather damage boost');
-				return move.basePower * 1.2;
-			}
-			this.debug('Light Feather NOT boosted');
-			return move.basePower;
-		}},
-		num: 1504.1,
-		gen: 3,
-		desc: "Boosts the power of a Flying-type holder by 1.2x if they move first.",
-	},
-	powergenerator: {
-		name: "Power Generator",
-		spritenum: 118,
-		fling: {
-			basePower: 30,
-		},
-		onStart(pokemon) {
-			if pokemon.hasType('Electric') {
-			pokemon.addVolatile('generating');
-			),
-		},
 		onEnd(pokemon) {
-			delete pokemon.volatiles['generating'];
-			this.add('-end', pokemon, 'generating', '[silent]');
+			pokemon.removeVolatile('drytorch');
 		},
 		condition: {
-			duration: 5,
+			noCopy: true, // doesn't get copied by Baton Pass
 			onStart(target) {
-				this.add('-start', target, 'item: Power Generator');
+				this.add('-start', target, 'item: Dry Torch');
 			},
 			onModifyAtkPriority: 5,
-			onModifyAtk(atk, pokemon) {
-				return this.chainModify(1);
+			onModifyAtk(atk, attacker, defender, move) {
+				if (move.type === 'Fire') {
+					this.debug('Dry Torch boost');
+					return this.chainModify(1.5);
+				}
+			},
+			onModifySpAPriority: 5,
+			onModifySpA(atk, attacker, defender, move) {
+				if (move.type === 'Fire') {
+					this.debug('Dry Torch boost');
+					return this.chainModify(1.5);
+				}
 			},
 			onEnd(target) {
-				this.boost{atk: 1, def: 1, spa: 1, spd: 1, spe: 1};
+				this.add('-end', target, 'ability: Dry Torch', '[silent]');
 			},
 		},
-		num: 1505.1,
-		gen: 5,
-		desc: "If an Electric-type holder stays on the field for 5 turns, it gains a +1 boost in all stats.",
-	},
-	oddscale: {
-		name: "Odd Scale",
-		spritenum: 108,
-		fling: {
-			basePower: 30,
-		},
-		onDamagingHit(damage, target, source, move) {
-			if (target !== source && move.category !== 'Status') {
-				if (pokemon.hasType('Dragon')) {
-					this.boost({atk: 1, spa: 1});
-				},
-			},
-		},
-		onDisableMove(pokemon) {
-			for (const moveSlot of pokemon.moveSlots) {
-				if (this.dex.getMove(moveSlot.move).category === 'Status') {
-					pokemon.disableMove(moveSlot.id);
-				}
-			}
-		},	
-		num: 1506.1,
+		num: 1502.1,
 		gen: 2,
-		desc: "If the holder is Dragon-type, they cannot use Status moves. However, they gain +1 Atk. and Sp.Atk whenever hit.",
+		desc: "If the holder is Fire-type and gets hit by a Fire-type move, the holder's Fire-type attacks have 1.5x power.",
 	},
 	brittlealloy: {
 		name: "Brittle Alloy",
@@ -132,5 +84,4 @@
 		gen: 4,
 		desc: "Switches the user out if they deal less than 10% with an attacking move.",
 	},
-
 };
