be67740d5a13d264ec63f0dc7e043004905d7020
DuoM2 <63012562+DuoM2@users.noreply.github.com>
2021-07-28 15:52:02 -0400

    Create typechart.ts

---
+++ data/mods/duomod/typechart.ts
@@ -0,0 +1,22 @@
+export const TypeChart: {[k: string]: ModdedTypeData} = {
+	Bug: 
+		damageTaken: {
+			Grass: 0,
+			Poison: 1,
+		},
+		HPivs: {},
+	},
+	Poison: 
+		damageTaken: {
+			Bug: 2,
+		},
+		HPivs: {},
+	},
+	Fairy: 
+		damageTaken: {
+			Bug: 2,
+			Dark: 0,
+		},
+		HPivs: {},
+	},
+};
