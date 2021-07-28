13acaf4dbaec317a1e801cb9986c4b48cdd5d981
DuoM2 <63012562+DuoM2@users.noreply.github.com>
2021-07-28 19:05:43 -0400

    Update moves.ts

--- data/mods/duomod/moves.ts
+++ data/mods/duomod/moves.ts
@@ -263,12 +263,12 @@ export const Moves: {[moveid: string]: MoveData} = {
 		condition: {
 			duration: 2,
 			onStart(pokemon, source) {
+					const damage = source.maxhp / 4;
 			},
 			onResidualOrder: 4,
 			onEnd(target) {
 				if (target && !target.fainted) {
 					target.cureStatus();
-					const damage = source.maxhp / -4;
 					if (damage) this.add('-heal', target, target.getHealth, '[from] move: Vibrant Light', '[wisher] ' + this.effectData.source.name);
 				}
 			},
