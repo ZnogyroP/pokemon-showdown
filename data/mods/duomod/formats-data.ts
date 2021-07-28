77b91e29e66a8473aa4b674f1a867010a8a8db75
DuoM2 <63012562+DuoM2@users.noreply.github.com>
2021-07-28 12:37:12 -0400

    Create formats-data.ts

---
+++ data/mods/duomod/formats-data.ts
@@ -0,0 +1,146 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	abysseil: {
+		tier: "Duomod",
+	},
+	annelait: {
+		tier: "Duomod",
+	},
+	arachwich: {
+		tier: "Duomod",
+	},
+	azurolt: {
+		tier: "Duomod",
+	},
+	baloon: {
+		tier: "Duomod",
+	},
+	batravolt: {
+		tier: "Duomod",
+	},
+	bismage: {
+		tier: "Duomod",
+	},
+	blastora: {
+		tier: "Duomod",
+	},
+	blaydge: {
+		tier: "Duomod",
+	},
+	cadbunny: {
+		tier: "Duomod",
+	},
+	castarupt: {
+		tier: "Duomod",
+	},
+	catelax: {
+		tier: "Duomod",
+	},
+	cephalopire: {
+		tier: "Duomod",
+	},
+	chemiclysm: {
+		tier: "Duomod",
+	},
+	chimida: {
+		tier: "Duomod",
+	},
+	chollunge: {
+		tier: "Duomod",
+	},
+	crazefly: {
+		tier: "Duomod",
+	},
+	crypterid: {
+		tier: "Duomod",
+	},
+	debring: {
+		tier: "Duomod",
+	},
+	deliriophage: {
+		tier: "Duomod",
+	},
+	detonuke: {
+		tier: "Duomod",
+	},
+	draglow: {
+		tier: "Duomod",
+	},
+	draxplosion: {
+		tier: "Duomod",
+	},
+	egg: {
+		tier: "Duomod",
+	},
+	eggcracked: {
+		tier: "Duomod",
+	},
+	electritar: {
+		tier: "Duomod",
+	},
+	falkick: {
+		tier: "Duomod",
+	},
+	fantom: {
+		tier: "Duomod",
+	},
+	flamepion: {
+		tier: "Duomod",
+	},
+	floundrawn: {
+		tier: "Duomod",
+	},
+	fluidrake: {
+		tier: "Duomod",
+	},
+	fluxtape: {
+		tier: "Duomod",
+	},
+	fluxtapestereo: {
+		tier: "Duomod",
+	},
+	grievenge: {
+		tier: "Duomod",
+	},
+	hyperoach: {
+		tier: "Duomod",
+	},
+	inscuba: {
+		tier: "Duomod",
+	},
+	joulibranch: {
+		tier: "Duomod",
+	},
+	lemotic: {
+		tier: "Duomod",
+	},
+	modolith: {
+		tier: "Duomod",
+	},
+	monstratus: {
+		tier: "Duomod",
+	},
+	mortemoth: {
+		tier: "Duomod",
+	},
+	nectrance: {
+		tier: "Duomod",
+	},
+	pterrost: {
+		tier: "Duomod",
+	},
+	sharmpedo: {
+		tier: "Duomod",
+	},
+	smorke: {
+		tier: "Duomod",
+	},
+	spirox: {
+		tier: "Duomod",
+	},
+	treemu: {
+		tier: "Duomod",
+	},
+	valianch: {
+		tier: "Duomod",
+	},
+};
