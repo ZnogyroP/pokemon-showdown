9b1be3a6c8cb39abcc390e5952665c5d062b7d95
Duo <varadyad@gmail.com>
2022-03-14 16:28:02 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1581,10 +1581,9 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 
 		var result: number;
 		this.hint("Time for the Roulette Wheel!");
-		result = this.random(6);
-		result = 2; // REMOVE LATER
+		result = this.random(40);
 	        if (result === 0) {
-		this.hint("Roulette Wheel Result: 1");
+			this.hint("Roulette Wheel Result: 1");
 	            for (const pokemon of this.getAllActive()) {
 	                this.heal(pokemon.maxhp, pokemon);
 	                pokemon.cureStatus();
@@ -1622,20 +1621,24 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 	        else if (result === 3) {
 			this.hint("Roulette Wheel Result: 4");
 			if (pickSide === 0) {
-				this.directDamage(this.sides[0].hp - 1, this.sides[0]);
+				for (const target of this.sides[0].pokemon) {
+					this.directDamage(pokemon.hp - 1, pokemon);
+				}
 			}
 			else if (pickSide === 1) {
-				this.directDamage(this.sides[1].hp - 1, this.sides[1]);
+				for (const target of this.sides[1].pokemon) {
+					this.directDamage(pokemon.hp - 1, pokemon);
+				}
 			}
 	        }
 	        else if (result === 4) {
-		this.hint("Roulette Wheel Result: 5");
+			this.hint("Roulette Wheel Result: 5");
 	            for (const pokemon of this.getAllActive()) {
 			this.directDamage(pokemon.hp, pokemon);
 		    }
 		}
 		else if (result === 5) {
-		this.hint("Roulette Wheel Result: 6");
+			this.hint("Roulette Wheel Result: 6");
 		    for (const pokemon of this.getAllActive()) {
 			this.useMove("Spikes", pokemon);
 			this.useMove("Spikes", pokemon);
@@ -1643,6 +1646,392 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			this.useMove("Charged Stone", pokemon);
 		    }
 		}
+		else if (result === 6) {
+			this.hint("Roulette Wheel Result: 7");
+			const result2 = this.random(3);
+			if (result2 === 0) {
+				this.field.setTerrain('grassyterrain');
+			} else if (result2 === 1) {
+				this.field.setTerrain('electricterrain');
+			} else {
+				this.field.setTerrain('mistyterrain');
+			}
+			const result3 = this.random(3);
+			if (result3 === 0) {
+				this.field.setWeather('sunnyday');
+			} else if (result3 === 1) {
+				this.field.setWeather('raindance');
+			} else {
+				this.field.setWeather('sandstorm');
+			}
+		}
+
+		else if (result === 7) {
+			this.hint("Roulette Wheel Result: 8");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.directDamage(1, pokemon);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.directDamage(1, pokemon);
+				}
+			}
+	        }
+
+		else if (result === 8) {
+			this.hint("Roulette Wheel Result: 9");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, pokemon, null, true);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, pokemon, null, true);
+				}
+			}
+	        } 
+
+		else if (result === 9) {
+			this.hint("Roulette Wheel Result: 10");
+			for (const pokemon of this.getAllActive()) {
+				pokemon.forceSwitchFlag = true;
+			}	
+		}
+
+		else if (result === 10) {
+			this.hint("Roulette Wheel Result: 11");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Conversion 2", pokemon);
+			}
+		}
+
+		else if (result === 11) {
+			this.hint("Roulette Wheel Result: 12");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Transform", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Transform", target);
+				}
+			}
+			
+		}
+
+		else if (result === 12) {
+			this.hint("Roulette Wheel Result: 13");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Heart Swap", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Heart Swap", target);
+				}
+			}
+			
+		}
+
+		else if (result === 13) {
+			this.hint("Roulette Wheel Result: 14");
+			for (const pokemon of this.getAllActive()) {
+				this.heal(pokemon.maxhp / 4, pokemon);
+	        	}
+	        } 
+
+		else if (result === 14) {
+			this.hint("Roulette Wheel Result: 15");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Scald 2", pokemon);
+			}
+		}
+
+		else if (result === 15) {
+			this.hint("Roulette Wheel Result: 16");
+			for (const pokemon of this.getAllActive()) {
+				pokemon.trySetStatus('tox', pokemon);
+	        	}
+		}
+
+		else if (result === 16) {
+			this.hint("Roulette Wheel Result: 17");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Court Change", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Court Change", target);
+				}
+			}	
+		}
+
+
+		else if (result === 17) {
+			this.hint("Roulette Wheel Result: 18");
+			for (const pokemon of this.getAllActive()) {
+		                this.boost({atk: 2, spa: 2}, pokemon);
+			}
+	        }
+
+		else if (result === 18) {
+			this.hint("Roulette Wheel Result: 19");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Camouflage", pokemon);
+			}
+		}
+
+		else if (result === 19) {
+			this.hint("Roulette Wheel Result: 20");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Skill Swap", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Skill Swap", target);
+				}
+			}	
+		}
+
+		else if (result === 20) {
+			this.hint("Roulette Wheel Result: 21");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Celebrate", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Celebrate", target);
+				}
+			}	
+		}
+
+		else if (result === 21) {
+			this.hint("Roulette Wheel Result: 22");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Trick Room", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Trick Room", target);
+				}
+			}	
+		}
+
+		else if (result === 22) {
+			this.hint("Roulette Wheel Result: 23");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					pokemon.addVolatile('confusion');
+					this.boost({accuracy: -12}, pokemon);	
+					const oldAbility = pokemon.setAbility('truant');
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					pokemon.addVolatile('confusion');
+					this.boost({accuracy: -12}, pokemon);	
+					const oldAbility = pokemon.setAbility('truant');
+				}
+			}	
+		}
+
+		else if (result === 23) {
+			this.hint("Roulette Wheel Result: 24");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Haze", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Haze", target);
+				}
+			}	
+		}
+
+		else if (result === 24) {
+			this.hint("Roulette Wheel Result: 25");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Magic Room", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Magic Room", target);
+				}
+			}	
+		}
+
+		else if (result === 25) {
+			this.hint("Roulette Wheel Result: 26");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Wonder Room", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Wonder Room", target);
+				}
+			}	
+		}
+
+		else if (result === 26) {
+			this.hint("Roulette Wheel Result: 27");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Pain Split", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Pain Split", target);
+				}
+			}	
+		}
+
+		else if (result === 27) {
+			this.hint("Roulette Wheel Result: 28");
+			for (const pokemon of this.getAllActive()) {
+	                	pokemon.cureStatus();
+	        	}
+	        }
+
+		else if (result === 28) {
+			this.hint("Roulette Wheel Result: 29");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Reflect", target);
+					this.useMove("Light Screen", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Reflect", target);
+					this.useMove("Light Screen", target);
+				}
+			}	
+		}			
+
+		else if (result === 29) {
+			this.hint("Roulette Wheel Result: 30");
+			for (const pokemon of this.getAllActive()) {
+	                	this.useMove("Safeguard", pokemon);
+	        	}
+	        }
+
+		else if (result === 30) {
+			this.hint("Roulette Wheel Result: 31");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Neutral Air", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Neutral Air", target);
+				}
+			}	
+		}
+
+		else if (result === 31) {
+			this.hint("Roulette Wheel Result: 32");
+			for (const pokemon of this.getAllActive()) {
+				pokemon.trySetStatus('frz', pokemon);
+	        	}
+		}
+
+		else if (result === 32) {
+			this.hint("Roulette Wheel Result: 33");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					target.forceSwitchFlag = true;
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					target.forceSwitchFlag = true;
+				}
+			}	
+		}
+
+		else if (result === 33) {
+			this.hint("Roulette Wheel Result: 34");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Water Shield", target);
+			}
+		}
+
+		else if (result === 34) {
+			this.hint("Roulette Wheel Result: 35");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Defog", target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Defog", target);
+				}
+			}
+		}
+
+		else if (result === 35) {
+			this.hint("Roulette Wheel Result: 36");
+			if (pickSide === 0) {
+				for (const target of this.sides[0].pokemon) {
+					this.useMove("Reflect Type, target);
+				}
+			}
+			else if (pickSide === 1) {
+				for (const target of this.sides[1].pokemon) {
+					this.useMove("Reflect Type", target);
+				}
+			}
+		}
+
+		else if (result === 36) {
+			this.hint("Roulette Wheel Result: 37");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Sheer Cold", target);
+			}
+		}
+
+		else if (result === 37) {
+			this.hint("Roulette Wheel Result: 38");
+			for (const pokemon of this.getAllActive()) {
+				pokemon.addVolatile('trapped', pokemon, pokemon, 'trapper');
+			}
+		}
+
+		else if (result === 38) {
+			this.hint("Roulette Wheel Result: 39");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Metronome", target);
+			}
+		}
+
+		else {
+			this.hint("Roulette Wheel Result: 40");
+			for (const pokemon of this.getAllActive()) {
+				this.useMove("Ultranome", target);
+			}
+		}
+
 
 	}
     },
