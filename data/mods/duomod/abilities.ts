c4d504a53e1693a596fa4a67486ec90cda4b7bbc
Duo <varadyad@gmail.com>
2022-03-15 00:44:54 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -674,35 +674,29 @@ disappearance: {
 		rating: 3,
 		num: 3004,
 	},
-	mentalnote: {
-		desc: "On switch-in, this Pokemon is alerted a random move known by an opposing Pokemon.",
-		shortDesc: "On switch-in, this Pokemon is alerted to a foes' move at random.",
-		onStart(pokemon) {
-			for (const target of pokemon.side.foe.active) {
-				const moveSlot of target.moveSlots;
-				const result = this.random(3);
-				const move = this.dex.getMove(moveSlot.move);
-				if (result === 0) {
-					let warnMoveName = this.dex.getMove(moveSlot[0].move);
-				}
-				else if (result === 1) {
-					let warnMoveName = this.dex.getMove(moveSlot[1].move);
-				}
-				else if (result === 2) {
-					let warnMoveName = this.dex.getMove(moveSlot[2].move);
-				}
-				else {
-					let warnMoveName = this.dex.getMove(moveSlot[3].move);
-				}
-					
+	mixitup: {
+		shortDesc: "If the user's attack doesn't match its last move, it's 1.3x stronger.",
+		onBasePower (basePower, pokemon, target, move) {
+			if (move !== pokemon.lastMove.id) {
+				return this.chainModify(1.3);
 			}
-			if (!warnMoves.length) return;
-			const [warnMoveName, warnTarget] = this.sample(warnMoves);
-			this.add('-activate', pokemon, 'ability: Mental Note', warnMoveName, '[of] ' + target);
 		},
-		name: "Mental Note",
+		name: "Mix it Up",
 		rating: 0.5,
-		num: 3005,
+		num: 3006,
+	},	
+	obtrusive: {
+		shortDesc: "Prevents the Roulette Wheel from being spun while active.",
+		onAnyTryMove(target, source, effect) {
+			if (['roulettespin'].includes(effect.id)) {
+				this.attrLastMove('[still]');
+				this.add('cant', this.effectData.target, 'ability: Obtrusive', effect, '[of] ' + target);
+				return false;
+			}
+		},
+		name: "Obtrusive",
+		rating: 1,
+		num: 3007,
 	},
 
 
