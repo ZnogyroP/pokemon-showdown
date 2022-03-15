e52a4468bc78ba81a33c3061aa55f2c713d9a279
Duo <varadyad@gmail.com>
2022-03-15 01:08:01 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -701,9 +701,7 @@ disappearance: {
 	overflow: {
 		shortDesc: "Uses Roulette Wheel twice after most status moves.",
 		onSourceHit(target, source, move) {
-			const noRedo: [
-					'roulettespin', 'spikes', 'chargedstone', 'neutralair', 'watershield', 'safeguard', 'lightscreen', 'reflect', 'grassyterrain', 'mistyterrain', 'electricterrain', 'sunnyday', 'raindance', 'sandstorm', 'conversion2', 'transform', 'heartswap', 'courtchange', 'camouflage', 'skillswap', 'trickroom', 'haze', 'magicroom', 'wonderroom', 'defog', 'reflecttype', 'metronome', 'ultranome',
-			],
+			
 
 			if (move.category === 'Status' && (effect.noRedo!.includes(move.name)) {
 				this.useMove("Roulette Spin", source);
