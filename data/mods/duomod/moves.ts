252c34eae747ab6113b2a41e149b106658539dfa
Duo <varadyad@gmail.com>
2022-03-15 16:41:14 -0400

    Update moves.ts

--- data/mods/duomod/moves.ts
+++ data/mods/duomod/moves.ts
@@ -83,7 +83,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		name: "Strike the Earth",
 		pp: 20,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1},
+		flags: {contact: 1, protect: 1, mirror: 1}, //ball
 		secondary: null,
 		target: "normal",
 		type: "Ground",
