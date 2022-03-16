755c86226b5bc3558729dc45a10b0b823e0dca1c
Duo <varadyad@gmail.com>
2022-03-16 10:35:59 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -310,161 +310,61 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	swagnetpull: {
 		shortDesc: "Prevents randomly-typed foes from choosing to switch.",
 		onFoeTrapPokemon(pokemon) {
-			let result = 0; // this.random(12);
+			const result = this.random(12);
 			if (result === 0) {
 				let currType = "Dark";
 				this.hint("Dark-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 1) {
 				let currType = "Grass";
 				this.hint("Grass-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 2) {
 				let currType = "Fire";
 				this.hint("Fire-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 3) {
 				let currType = "Water";
 				this.hint("Water-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 4) {
 				let currType = "Electric";
 				this.hint("Electric-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 5) {
 				let currType = "Ground";
 				this.hint("Ground-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-				}
-				else {
-					this.hint("Nope.");
-					return pokemon.removeVolatile('trapped', source, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 6) {
 				let currType = "Flying";
 				this.hint("Flying-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 7) {
 				let currType = "Dragon";
 				this.hint("Dragon-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 8) {
 				let currType = "Fairy";
 				this.hint("Fairy-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 9) {
 				let currType = "Steel";
 				this.hint("Steel-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 10) {
 				let currType = "Bug";
 				this.hint("Bug-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
 			}
 			else if (result === 11) {
 				let currType = "Poison";
 				this.hint("Poison-types are now being trapped.");
-				if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
-					pokemon.addVolatile('trapped', pokemon, null, 'trapper');							pokemon.tryTrap(true);
-					result = 13;
-				}
-				else {
-					this.hint("Nope.");
-					pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
-					pokemon.tryTrap(false);
-				}
+			}
+			if (pokemon.hasType(currType) && this.isAdjacent(pokemon, this.effectData.target)) {
+				pokemon.addVolatile('trapped', pokemon, null, 'trapper');
+			}
+			else {
+				this.hint("Nope.");
+				pokemon.removeVolatile('trapped', pokemon, null, 'trapper');
 			}
 		},
 		onFoeMaybeTrapPokemon(pokemon, source) {
