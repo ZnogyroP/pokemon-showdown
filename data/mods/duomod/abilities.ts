89251297adeffb2d9d3ea796a738718176de8003
Duo <varadyad@gmail.com>
2022-03-16 13:31:41 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -160,7 +160,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	dropheat: {
 		desc: "This Pokemon's Fire-type and Sound-based moves become stronger if it attacks and knocks out another Pokemon.",
 		shortDesc: "This Pokemon's Sound + Fire moves strengthen one KOs another Pokemon.",
-		let amped = 0;
+		static amped: number = 0;
 		onSourceAfterFaint(length, target, source, effect) {
 			if (effect && effect.effectType === 'Move') {
 				if (source.lastMove.type === 'Fire' || source.lastMove.flags['sound']) {
