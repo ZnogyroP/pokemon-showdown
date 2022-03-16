84879460d498ebfff1fd8ca8f0ef214c1de3f371
Duo <varadyad@gmail.com>
2022-03-16 11:49:42 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -368,7 +368,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			pokemon.maybeTrapped = true;
 		},
 		name: "Swagnet Pull",
-		rating: 4,
+		rating: 4, // <- on purpose 
 		num: 20,
 	},
 	toughout: {
