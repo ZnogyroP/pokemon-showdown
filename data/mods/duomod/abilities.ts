bb97f382ecb78200e9304fb371d81d02b9b46b1a
Duo <varadyad@gmail.com>
2022-03-16 10:57:04 -0400

    Update abilities.ts

--- data/mods/duomod/abilities.ts
+++ data/mods/duomod/abilities.ts
@@ -365,7 +365,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			}
 			else {
 				pokemon.tryTrap(false);
-			} //
+			}
 		},
 		onFoeMaybeTrapPokemon(pokemon, source) {
 			pokemon.maybeTrapped = true;
