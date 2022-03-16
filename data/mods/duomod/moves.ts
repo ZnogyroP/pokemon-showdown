0152b6863426cd58493f930b8a92904e51a31110
Duo <varadyad@gmail.com>
2022-03-16 06:05:45 -0400

    effect descs

--- data/mods/duomod/moves.ts
+++ data/mods/duomod/moves.ts
@@ -225,18 +225,23 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		onHit(source) {
 		var result: number;
 		var pickSide: number;
-		this.hint("Let's spin the Wheel!");
+		for (const allPokemon of this.getAllActive()) {
+			if (allPokemon.hasAbility('obtrusive')) {
+				return;
+			}
+		} 
+		this.hint("Time for a bonus wheel!");
 		result = this.random(40);
 	        pickSide = this.random(2);
 		if (result === 0) {
-			this.hint("Roulette Wheel Result: 1");
+			this.hint("Roulette Wheel Result 1 - Fully heal every active Pokemon.");
 	            for (const pokemon of this.getAllActive()) {
 	                this.heal(pokemon.maxhp, pokemon);
 	                pokemon.cureStatus();
 	            }
 	        } 
 	        else if (result === 1) {
-			this.hint("Roulette Wheel Result: 2");
+			this.hint("Roulette Wheel Result 2 - Greatly increase everyone's highest stat.");
 	            for (const pokemon of this.getAllActive()) {
 	                let statName = 'atk';
 	                let bestStat = 0;
@@ -252,7 +257,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 	        } 
 
 	        else if (result === 2) {
-			this.hint("Roulette Wheel Result: 3");
+			this.hint("Roulette Wheel Result 3 - Give one Pokemon an omniboost.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -269,7 +274,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 			}
 	        } 
 	        else if (result === 3) {
-			this.hint("Roulette Wheel Result: 4");
+			this.hint("Roulette Wheel Result 4 - Set one Pokemon to 1 HP.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -286,20 +291,20 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 			}
 	        }
 	        else if (result === 4) {
-			this.hint("Roulette Wheel Result: 5");
+			this.hint("Roulette Wheel Result 5 - screw you both");
 	            for (const pokemon of this.getAllActive()) {
 			this.directDamage(pokemon.hp, pokemon);
 		    }
 		}
 		else if (result === 5) {
-			this.hint("Roulette Wheel Result: 6");
+			this.hint("Roulette Wheel Result 6 - Set hazards on both sides.");
 		    for (const pokemon of this.getAllActive()) {
 			this.useMove("Spikes", pokemon);
 			this.useMove("Charged Stone", pokemon);
 		    }
 		}
 		else if (result === 6) {
-			this.hint("Roulette Wheel Result: 7");
+			this.hint("Roulette Wheel Result 7 - Set a random weather and terrain.");
 			const result2 = this.random(3);
 			const result3 = this.random(3);
 			if (pickSide === 0) {
@@ -345,7 +350,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 7) {
-			this.hint("Roulette Wheel Result: 8");
+			this.hint("Roulette Wheel Result 8 - lmao");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -363,7 +368,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 	        }
 
 		else if (result === 8) {
-			this.hint("Roulette Wheel Result: 9");
+			this.hint("Roulette Wheel Result 9 - Minimize every stat of one Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -381,21 +386,21 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 	        } 
 
 		else if (result === 9) {
-			this.hint("Roulette Wheel Result: 10");
+			this.hint("Roulette Wheel Result 10 - Forcibly switch every Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.forceSwitchFlag = true;
 			}	
 		}
 
 		else if (result === 10) {
-			this.hint("Roulette Wheel Result: 11");
+			this.hint("Roulette Wheel Result 11 - Make every Pokemon use Conversion 2.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Conversion 2", pokemon);
 			}
 		}
 
 		else if (result === 11) {
-			this.hint("Roulette Wheel Result: 12");
+			this.hint("Roulette Wheel Result 12 - Make one Pokemon Transform into the other.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -414,7 +419,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 12) {
-			this.hint("Roulette Wheel Result: 13");
+			this.hint("Roulette Wheel Result 13 - Make both Pokemon trade stat changes.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -432,28 +437,28 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 13) {
-			this.hint("Roulette Wheel Result: 14");
+			this.hint("Roulette Wheel Result 14 - Slightly heal both Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				this.heal(pokemon.maxhp / 4, pokemon);
 	        	}
 	        } 
 
 		else if (result === 14) {
-			this.hint("Roulette Wheel Result: 15");
+			this.hint("Roulette Wheel Result 15 - heard you guys liked scald");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Scald 2", pokemon);
 			}
 		}
 
 		else if (result === 15) {
-			this.hint("Roulette Wheel Result: 16");
+			this.hint("Roulette Wheel Result 16 - Attempt to Toxic both Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.trySetStatus('tox', pokemon);
 	        	}
 		}
 
 		else if (result === 16) {
-			this.hint("Roulette Wheel Result: 17");
+			this.hint("Roulette Wheel Result 17 - Switch both sides' field effects.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -472,21 +477,21 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 
 
 		else if (result === 17) {
-			this.hint("Roulette Wheel Result: 18");
+			this.hint("Roulette Wheel Result 18 - Raise both active Pokemons' attacking stats.");
 			for (const pokemon of this.getAllActive()) {
 		                this.boost({atk: 2, spa: 2}, pokemon);
 			}
 	        }
 
 		else if (result === 18) {
-			this.hint("Roulette Wheel Result: 19");
+			this.hint("Roulette Wheel Result 19 - Make both Pokemon use Camouflage.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Camouflage", pokemon);
 			}
 		}
 
 		else if (result === 19) {
-			this.hint("Roulette Wheel Result: 20");
+			this.hint("Roulette Wheel Result 20 - Make both Pokemon swap abilities.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -504,7 +509,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 20) {
-			this.hint("Roulette Wheel Result: 21");
+			this.hint("Roulette Wheel Result 21 - wahoo");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -522,7 +527,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 21) {
-			this.hint("Roulette Wheel Result: 22");
+			this.hint("Roulette Wheel Result 22 - Sets Trick Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -540,7 +545,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 22) {
-			this.hint("Roulette Wheel Result: 23");
+			this.hint("Roulette Wheel Result 23 - Sand Attack go!");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -558,7 +563,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 23) {
-			this.hint("Roulette Wheel Result: 24");
+			this.hint("Roulette Wheel Result 24 - Removes all active stat changes.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -576,7 +581,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 24) {
-			this.hint("Roulette Wheel Result: 25");
+			this.hint("Roulette Wheel Result 25 - Sets Magic Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -594,7 +599,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 25) {
-			this.hint("Roulette Wheel Result: 26");
+			this.hint("Roulette Wheel Result 26 - Sets Wonder Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -612,7 +617,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 26) {
-			this.hint("Roulette Wheel Result: 27");
+			this.hint("Roulette Wheel Result 27 - Averages out the HP of active Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -630,14 +635,14 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 27) {
-			this.hint("Roulette Wheel Result: 28");
+			this.hint("Roulette Wheel Result 28 - Cures all active Pokemons' statuses.");
 			for (const pokemon of this.getAllActive()) {
 	                	pokemon.cureStatus();
 	        	}
 	        }
 
 		else if (result === 28) {
-			this.hint("Roulette Wheel Result: 29");
+			this.hint("Roulette Wheel Result 29 - Sets up Screens for one side.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -657,14 +662,14 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}			
 
 		else if (result === 29) {
-			this.hint("Roulette Wheel Result: 30");
+			this.hint("Roulette Wheel Result 30 - Starts a status immunity for both sides.");
 			for (const pokemon of this.getAllActive()) {
 	                	this.useMove("Safeguard", pokemon);
 	        	}
 	        }
 
 		else if (result === 30) {
-			this.hint("Roulette Wheel Result: 31");
+			this.hint("Roulette Wheel Result 31 - Deactivates all abilities that are active within 5 turns.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -682,14 +687,14 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 31) {
-			this.hint("Roulette Wheel Result: 32");
+			this.hint("Roulette Wheel Result 32 - Attempts to Freeze all active Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.trySetStatus('frz', pokemon);
 	        	}
 		}
 
 		else if (result === 32) {
-			this.hint("Roulette Wheel Result: 33");
+			this.hint("Roulette Wheel Result 33 - Switches out one Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -707,14 +712,14 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 33) {
-			this.hint("Roulette Wheel Result: 34");
+			this.hint("Roulette Wheel Result 34 - Sets up Water Shield for both sides.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Water Shield", pokemon);
 			}
 		}
 
 		else if (result === 34) {
-			this.hint("Roulette Wheel Result: 35");
+			this.hint("Roulette Wheel Result 35 - One active Pokemon Defogs.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -732,7 +737,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 35) {
-			this.hint("Roulette Wheel Result: 36");
+			this.hint("Roulette Wheel Result 36 - Both active Pokemon share a type combination.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -750,28 +755,28 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		}
 
 		else if (result === 36) {
-			this.hint("Roulette Wheel Result: 37");
+			this.hint("Roulette Wheel Result 37 - glhf");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Sheer Cold", pokemon);
 			}
 		}
 
 		else if (result === 37) {
-			this.hint("Roulette Wheel Result: 38");
+			this.hint("Roulette Wheel Result 38 - uh oh");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.addVolatile('trapped', pokemon, pokemon, 'trapper');
 			}
 		}
 
 		else if (result === 38) {
-			this.hint("Roulette Wheel Result: 39");
+			this.hint("Roulette Wheel Result 39 - Both active Pokemon use Metronome.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Metronome", pokemon);
 			}
 		}
 
 		else {
-			this.hint("Roulette Wheel Result: 40");
+			this.hint("Roulette Wheel Result 40 - THE ULTIMATE EFFECT");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Ultranome", pokemon);
 			}
@@ -1708,6 +1713,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		pp: 5,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
+		hasCrashDamage: true,
 		onAfterMoveSecondarySelf(pokemon, target, move) {
 			if (!target || target.fainted || target.hp <= 0) {
 				this.hint("uh oh");
--- data/mods/duomod/rulesets.ts
+++ data/mods/duomod/rulesets.ts
@@ -4,7 +4,7 @@ export const Formats: {[k: string]: FormatData} = {
 		name: 'Subscribe For More Content',
 		desc: 'https://www.youtube.com/channel/UCvVihnVokWwZ4NpeMsBk48A',
 		onResidual(pokemon) {
-        	var sideChoice: number;
+       		var sideChoice: number;
 		const pickSide = this.random(2);
 		for (const allPokemon of this.getAllActive()) {
 			if (allPokemon.hasAbility('obtrusive')) {
@@ -18,14 +18,14 @@ export const Formats: {[k: string]: FormatData} = {
 		result = this.random(40);	
 
 		if (result === 0) {
-			this.hint("Roulette Wheel Result: 1");
+			this.hint("Roulette Wheel Result 1 - Fully heal every active Pokemon.");
 	            for (const pokemon of this.getAllActive()) {
 	                this.heal(pokemon.maxhp, pokemon);
 	                pokemon.cureStatus();
 	            }
 	        } 
 	        else if (result === 1) {
-			this.hint("Roulette Wheel Result: 2");
+			this.hint("Roulette Wheel Result 2 - Greatly increase everyone's highest stat.");
 	            for (const pokemon of this.getAllActive()) {
 	                let statName = 'atk';
 	                let bestStat = 0;
@@ -41,24 +41,24 @@ export const Formats: {[k: string]: FormatData} = {
 	        } 
 
 	        else if (result === 2) {
-			this.hint("Roulette Wheel Result: 3");
+			this.hint("Roulette Wheel Result 3 - Give one Pokemon an omniboost.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
-					this.boost({atk: 1, def: 1, spa: 1, spd: 1, spe: 1}, target, pokemon, null, true);
+					this.boost({atk: 1, def: 1, spa: 1, spd: 1, spe: 1}, target, target, null, true);
 				}
 				}
 			}
 			else if (pickSide === 1) {
 				for (const target of this.sides[1].pokemon) {
 				if (target.isActive) {
-					this.boost({atk: 1, def: 1, spa: 1, spd: 1, spe: 1}, target, pokemon, null, true);
+					this.boost({atk: 1, def: 1, spa: 1, spd: 1, spe: 1}, target, target, null, true);
 				}
 				}
 			}
 	        } 
 	        else if (result === 3) {
-			this.hint("Roulette Wheel Result: 4");
+			this.hint("Roulette Wheel Result 4 - Set one Pokemon to 1 HP.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -75,20 +75,20 @@ export const Formats: {[k: string]: FormatData} = {
 			}
 	        }
 	        else if (result === 4) {
-			this.hint("Roulette Wheel Result: 5");
+			this.hint("Roulette Wheel Result 5 - screw you both");
 	            for (const pokemon of this.getAllActive()) {
 			this.directDamage(pokemon.hp, pokemon);
 		    }
 		}
 		else if (result === 5) {
-			this.hint("Roulette Wheel Result: 6");
+			this.hint("Roulette Wheel Result 6 - Set hazards on both sides.");
 		    for (const pokemon of this.getAllActive()) {
 			this.useMove("Spikes", pokemon);
 			this.useMove("Charged Stone", pokemon);
 		    }
 		}
 		else if (result === 6) {
-			this.hint("Roulette Wheel Result: 7");
+			this.hint("Roulette Wheel Result 7 - Set a random weather and terrain.");
 			const result2 = this.random(3);
 			const result3 = this.random(3);
 			if (pickSide === 0) {
@@ -134,7 +134,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 7) {
-			this.hint("Roulette Wheel Result: 8");
+			this.hint("Roulette Wheel Result 8 - lmao");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -152,39 +152,39 @@ export const Formats: {[k: string]: FormatData} = {
 	        }
 
 		else if (result === 8) {
-			this.hint("Roulette Wheel Result: 9");
+			this.hint("Roulette Wheel Result 9 - Minimize every stat of one Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
-					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, pokemon, null, true);
+					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, target, null, true);
 				}
 				}
 			}
 			else if (pickSide === 1) {
 				for (const target of this.sides[1].pokemon) {
 				if (target.isActive) {
-					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, pokemon, null, true);
+					this.boost({atk: -12, def: -12, spa: -12, spd: -12, spe: -12}, target, target, null, true);
 				}
 				}
 			}
 	        } 
 
 		else if (result === 9) {
-			this.hint("Roulette Wheel Result: 10");
+			this.hint("Roulette Wheel Result 10 - Forcibly switch every Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.forceSwitchFlag = true;
 			}	
 		}
 
 		else if (result === 10) {
-			this.hint("Roulette Wheel Result: 11");
+			this.hint("Roulette Wheel Result 11 - Make every Pokemon use Conversion 2.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Conversion 2", pokemon);
 			}
 		}
 
 		else if (result === 11) {
-			this.hint("Roulette Wheel Result: 12");
+			this.hint("Roulette Wheel Result 12 - Make one Pokemon Transform into the other.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -203,7 +203,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 12) {
-			this.hint("Roulette Wheel Result: 13");
+			this.hint("Roulette Wheel Result 13 - Make both Pokemon trade stat changes.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -221,28 +221,28 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 13) {
-			this.hint("Roulette Wheel Result: 14");
+			this.hint("Roulette Wheel Result 14 - Slightly heal both Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				this.heal(pokemon.maxhp / 4, pokemon);
 	        	}
 	        } 
 
 		else if (result === 14) {
-			this.hint("Roulette Wheel Result: 15");
+			this.hint("Roulette Wheel Result 15 - heard you guys liked scald");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Scald 2", pokemon);
 			}
 		}
 
 		else if (result === 15) {
-			this.hint("Roulette Wheel Result: 16");
+			this.hint("Roulette Wheel Result 16 - Attempt to Toxic both Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.trySetStatus('tox', pokemon);
 	        	}
 		}
 
 		else if (result === 16) {
-			this.hint("Roulette Wheel Result: 17");
+			this.hint("Roulette Wheel Result 17 - Switch both sides' field effects.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -261,21 +261,21 @@ export const Formats: {[k: string]: FormatData} = {
 
 
 		else if (result === 17) {
-			this.hint("Roulette Wheel Result: 18");
+			this.hint("Roulette Wheel Result 18 - Raise both active Pokemons' attacking stats.");
 			for (const pokemon of this.getAllActive()) {
 		                this.boost({atk: 2, spa: 2}, pokemon);
 			}
 	        }
 
 		else if (result === 18) {
-			this.hint("Roulette Wheel Result: 19");
+			this.hint("Roulette Wheel Result 19 - Make both Pokemon use Camouflage.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Camouflage", pokemon);
 			}
 		}
 
 		else if (result === 19) {
-			this.hint("Roulette Wheel Result: 20");
+			this.hint("Roulette Wheel Result 20 - Make both Pokemon swap abilities.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -293,7 +293,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 20) {
-			this.hint("Roulette Wheel Result: 21");
+			this.hint("Roulette Wheel Result 21 - wahoo");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -311,7 +311,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 21) {
-			this.hint("Roulette Wheel Result: 22");
+			this.hint("Roulette Wheel Result 22 - Sets Trick Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -329,7 +329,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 22) {
-			this.hint("Roulette Wheel Result: 23");
+			this.hint("Roulette Wheel Result 23 - Sand Attack go!");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -347,7 +347,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 23) {
-			this.hint("Roulette Wheel Result: 24");
+			this.hint("Roulette Wheel Result 24 - Removes all active stat changes.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -365,7 +365,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 24) {
-			this.hint("Roulette Wheel Result: 25");
+			this.hint("Roulette Wheel Result 25 - Sets Magic Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -383,7 +383,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 25) {
-			this.hint("Roulette Wheel Result: 26");
+			this.hint("Roulette Wheel Result 26 - Sets Wonder Room.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -401,7 +401,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 26) {
-			this.hint("Roulette Wheel Result: 27");
+			this.hint("Roulette Wheel Result 27 - Averages out the HP of active Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -419,14 +419,14 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 27) {
-			this.hint("Roulette Wheel Result: 28");
+			this.hint("Roulette Wheel Result 28 - Cures all active Pokemons' statuses.");
 			for (const pokemon of this.getAllActive()) {
 	                	pokemon.cureStatus();
 	        	}
 	        }
 
 		else if (result === 28) {
-			this.hint("Roulette Wheel Result: 29");
+			this.hint("Roulette Wheel Result 29 - Sets up Screens for one side.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -446,14 +446,14 @@ export const Formats: {[k: string]: FormatData} = {
 		}			
 
 		else if (result === 29) {
-			this.hint("Roulette Wheel Result: 30");
+			this.hint("Roulette Wheel Result 30 - Starts a status immunity for both sides.");
 			for (const pokemon of this.getAllActive()) {
 	                	this.useMove("Safeguard", pokemon);
 	        	}
 	        }
 
 		else if (result === 30) {
-			this.hint("Roulette Wheel Result: 31");
+			this.hint("Roulette Wheel Result 31 - Deactivates all abilities that are active within 5 turns.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -471,14 +471,14 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 31) {
-			this.hint("Roulette Wheel Result: 32");
+			this.hint("Roulette Wheel Result 32 - Attempts to Freeze all active Pokemon.");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.trySetStatus('frz', pokemon);
 	        	}
 		}
 
 		else if (result === 32) {
-			this.hint("Roulette Wheel Result: 33");
+			this.hint("Roulette Wheel Result 33 - Switches out one Pokemon.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -496,14 +496,14 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 33) {
-			this.hint("Roulette Wheel Result: 34");
+			this.hint("Roulette Wheel Result 34 - Sets up Water Shield for both sides.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Water Shield", pokemon);
 			}
 		}
 
 		else if (result === 34) {
-			this.hint("Roulette Wheel Result: 35");
+			this.hint("Roulette Wheel Result 35 - One active Pokemon Defogs.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -521,7 +521,7 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 35) {
-			this.hint("Roulette Wheel Result: 36");
+			this.hint("Roulette Wheel Result 36 - Both active Pokemon share a type combination.");
 			if (pickSide === 0) {
 				for (const target of this.sides[0].pokemon) {
 				if (target.isActive) {
@@ -539,32 +539,32 @@ export const Formats: {[k: string]: FormatData} = {
 		}
 
 		else if (result === 36) {
-			this.hint("Roulette Wheel Result: 37");
+			this.hint("Roulette Wheel Result 37 - glhf");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Sheer Cold", pokemon);
 			}
 		}
 
 		else if (result === 37) {
-			this.hint("Roulette Wheel Result: 38");
+			this.hint("Roulette Wheel Result 38 - uh oh");
 			for (const pokemon of this.getAllActive()) {
 				pokemon.addVolatile('trapped', pokemon, pokemon, 'trapper');
 			}
 		}
 
 		else if (result === 38) {
-			this.hint("Roulette Wheel Result: 39");
+			this.hint("Roulette Wheel Result 39 - Both active Pokemon use Metronome.");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Metronome", pokemon);
 			}
 		}
 
 		else {
-			this.hint("Roulette Wheel Result: 40");
+			this.hint("Roulette Wheel Result 40 - THE ULTIMATE EFFECT");
 			for (const pokemon of this.getAllActive()) {
 				this.useMove("Ultranome", pokemon);
 			}
-		} 
-		},  
+		}
+		},
 	},		
 };
