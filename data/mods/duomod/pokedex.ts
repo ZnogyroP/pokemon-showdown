e2593a0777ef6c43b214982e98c4b7552f094bcf
Duo <63012562+DuoM2@users.noreply.github.com>
2022-03-26 02:43:30 -0400

    speedy

--- data/mods/duomod/pokedex.ts
+++ data/mods/duomod/pokedex.ts
@@ -4,7 +4,7 @@ abysseil: {
 	species: "Abysseil",
 	types: ["Water", "Fairy"],
 	gender: "N",
-	baseStats: {hp: 80, atk: 44, def: 95, spa: 83, spd: 95, spe: 138},
+	baseStats: {hp: 80, atk: 44, def: 95, spa: 83, spd: 95, spe: 158},
 	abilities: {0: "Sap Sipper"},
 	heightm: 1,
 	weightkg: 5,
