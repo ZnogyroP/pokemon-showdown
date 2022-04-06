5451f3bee04cee8d47131cab12eed43e3e68c550
Duo <63012562+DuoM2@users.noreply.github.com>
2022-04-06 14:29:37 -0400

    Create statuses.ts

---
+++ data/statuses.ts
@@ -0,0 +1,32 @@
+export const Statuses: {[k: string]: ModdedPureEffectData} = {
+  frz: {
+		name: 'frz',
+		inherit: true,
+    onStart(target, source, sourceEffect) {
+      this.effectData.time = 4;
+    }
+		onBeforeMove(pokemon, target, move) {
+      pokemon.statusData.time--;
+			if (pokemon.statusData.time <= 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			if (move.flags['defrost']) return;
+			this.add('cant', pokemon, 'frz');
+			return false;
+		},
+		onModifyMove() {},
+		onHit() {},
+		onAfterMoveSecondary(target, source, move) {
+			if ((move.secondary && move.secondary.status === 'brn') || move.statusRoll === 'brn') {
+				target.cureStatus();
+			}
+		},
+		onAfterMoveSecondarySelf(pokemon, target, move) {
+			if (move.flags['defrost']) pokemon.cureStatus();
+		},
+		onResidual(pokemon) {
+			if (this.randomChance(40, 100)) pokemon.cureStatus();
+		},
+	},
+};
