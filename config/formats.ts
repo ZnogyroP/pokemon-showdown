99a90917c69bc8db6c866d24c0df29778a9ca943
Duo <63012562+DuoM2@users.noreply.github.com>
2022-06-03 11:29:42 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -26,7 +26,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 			onSwitchIn(pokemon) {
         		this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
         	},	
-	}, //
+	}, 
 	
 	{
 		name: "[Gen 8] Duomod Randbats",
@@ -51,6 +51,22 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
         	banlist: ['Uber'],
         	
 	},
+	
+	
+	{
+        name: "[Gen 8] Patratdex",
+        desc: `<b>Patratdex</b>: Galvantic's Solomod, containing a new regional dex with 151 Fakemon and some new abilities.`,
+        mod: 'patrtdex',
+        ruleset: ['Standard NatDex', 'Dynamax Clause', 'Sleep Clause Mod', 'Species Clause', 'Moody Clause', 'Evasion Moves Clause', 'Swagger Clause', 'Baton Pass Clause', 'OHKO Clause', 'Subscribe For More Content', 'Duomod Data Mod'],
+        banlist: ['All Pokemon', 'Normalium Z', 'Fairium Z', 'Fightinium Z', 'Firium Z', 'Flyinium Z', 'Darkinium Z', 'Dragonium Z', 'Buginium Z', 'Waterium Z', 'Electrium Z', 'Ghostium Z', 'Grassium Z', 'Groundium Z', 'Icium Z', 'Poisonium Z', 'Psychium Z', 'Rockium Z', 'Steelium Z',],
+        unbanlist: [
+             'Patratdex OU', 'Patratdex NFE', 'Patratdex LC', 
+			],	
+			onSwitchIn(pokemon) {
+        		this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
+        	},	
+	},
+	
 	{
 		name: "[Gen 8] Random Battle",
 		desc: `Randomized teams of level-balanced Pok&eacute;mon with sets that are generated to be competitively viable.`,
