d7e2d611990cc6b94880c0a9bc01bac4c60b207d
Duo <63012562+DuoM2@users.noreply.github.com>
2022-06-03 17:19:17 -0400

    Update rulesets.ts

--- data/mods/patratdex/rulesets.ts
+++ data/mods/patratdex/rulesets.ts
@@ -6,7 +6,7 @@ export const Formats: {[k: string]: FormatData} = {
 		onValidateSet(set) {
 			const species = this.dex.getSpecies(set.species);
 			const exceptions = ["onix", "onixpatratdex", "wobbuffet", "wobbuffetpatratdex", "spoink", "spoinkpatratdex", "wynaut", "wynautpatratdex", "luvdisc", "luvdiscpatratdex", "carnivine", "carnivinepatratdex", "patrat", "patratpatratdex", "watchog", "watchogpatratdex", "vanillite", "valillitepatratdex", "vanillish", "vanillishpatratdex", "vanilluxe", "vanilluxepatratdex", "litwick", "litwickpatratdex", "lampent", "lampentpatratdex", "chandelure", "chandelurepatratdex",]
-			if ((exceptions!.includes(species.name) || (species.num < 1000)) {
+			if ((exceptions!.includes(species.name)) || (species.num < 1000)) {
         return [
 							"Previously-existing Pokemon are banned. Please use the Pokemon created for this mod.",
 						];
