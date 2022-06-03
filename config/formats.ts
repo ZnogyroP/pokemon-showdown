e4de599df2ee8e5bde4ad0160762ce444455472d
Duo <63012562+DuoM2@users.noreply.github.com>
2022-06-03 11:37:23 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -56,11 +56,11 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 	{
         name: "[Gen 8] Patratdex",
         desc: `<b>Patratdex</b>: Galvantic's Solomod, containing a new regional dex with 151 Fakemon and some new abilities.`,
-        mod: 'patrtdex',
+        mod: 'patratdex',
         ruleset: ['Standard NatDex', 'Dynamax Clause', 'Sleep Clause Mod', 'Species Clause', 'Moody Clause', 'Evasion Moves Clause', 'Swagger Clause', 'Baton Pass Clause', 'OHKO Clause', 'Subscribe For More Content', 'Duomod Data Mod'],
         banlist: ['All Pokemon', 'Normalium Z', 'Fairium Z', 'Fightinium Z', 'Firium Z', 'Flyinium Z', 'Darkinium Z', 'Dragonium Z', 'Buginium Z', 'Waterium Z', 'Electrium Z', 'Ghostium Z', 'Grassium Z', 'Groundium Z', 'Icium Z', 'Poisonium Z', 'Psychium Z', 'Rockium Z', 'Steelium Z',],
         unbanlist: [
-             'Berribat', 
+             'Patratdex OU', 
 			],	
 			onSwitchIn(pokemon) {
         		this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
