7255fd482218ae79110d3c0944b6a35b0b2ff06b
Duo <63012562+DuoM2@users.noreply.github.com>
2022-07-02 01:58:00 -0400

    Update moves.ts

--- data/mods/expansions/moves.ts
+++ data/mods/expansions/moves.ts
@@ -1,4 +1,17 @@
 export const Moves: {[moveid: string]: ModdedMoveData} = {
+	
+	"gastroacid": {
+		inherit: true,
+		condition: {
+			// Ability suppression implemented in Pokemon.ignoringAbility() within sim/pokemon.js
+			onStart(pokemon) {
+				this.add('-endability', pokemon);
+				this.singleEvent('End', pokemon.getAbility(), pokemon.abilityData, pokemon, pokemon, 'gastroacid');
+				if (pokemon.m.innates) (pokemon.m.innates as string[]).forEach(innate => pokemon.removeVolatile("ability" + innate));
+			},
+		},
+	},
+	
 	replicate: {
 		num: 3001,
 		accuracy: 100,
