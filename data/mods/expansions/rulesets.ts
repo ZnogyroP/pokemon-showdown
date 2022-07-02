b13712ea6c285582f02572690dbe92de4827b05e
Duo <63012562+DuoM2@users.noreply.github.com>
2022-07-02 04:08:29 -0400

    Update rulesets.ts

--- data/mods/expansions/rulesets.ts
+++ data/mods/expansions/rulesets.ts
@@ -5,7 +5,7 @@ export const Formats: {[k: string]: FormatData} = {
 		desc: 'Adds a fifth move to every Pokemon.',
     onBegin() {
       for (const side0 of this.sides[0].pokemon) { // i had to do both sides individually from lack of knowledge in this language, if anyone knows of a way to condense this then feel free to let me know
-			side0.moveSlots[side0.moveSlots.length] = {
+			side0.baseMoveSlots[side0.moveSlots.length] = {
 	  			move: "Fifth Move",
       		id: "fifthmove",
       		pp: 1,
@@ -14,11 +14,10 @@ export const Formats: {[k: string]: FormatData} = {
       		disabled: false,
        		used: false,
      		};
-			side0.baseMoveSlots[side0.baseMoveSlots.length] = side0.moveSlots[side0.moveSlots.length - 1]
 		}
 
       for (const side1 of this.sides[1].pokemon) { // i had to do both sides individually from lack of knowledge in this language, if anyone knows of a way to condense this then feel free to let me know
-			side1.moveSlots[side1.moveSlots.length] = {
+			side1.baseMoveSlots[side1.moveSlots.length] = {
 	  			move: "Fifth Move",
       		id: "fifthmove",
       		pp: 1,
@@ -27,7 +26,6 @@ export const Formats: {[k: string]: FormatData} = {
       		disabled: false,
        		used: false,
      		};        
-			side1.baseMoveSlots[side1.baseMoveSlots.length] = side1.moveSlots[side0.moveSlots.length - 1]
 		}
 	 },
 	},
