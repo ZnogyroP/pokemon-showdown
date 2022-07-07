871d98b1a39ba062fc771aea040afeebeb497b23
Duo <63012562+DuoM2@users.noreply.github.com>
2022-07-07 02:19:14 -0400

    Create formats-data.ts

---
+++ data/mods/mememons/formats-data.ts
@@ -0,0 +1,34 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	kermiitthefrog: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+  goku: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+wallace: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+gromit: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+spongebob: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+darthvader: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	}, 
+hotelmario: {
+  tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+waluigi: {
+		tier: "Mememons",
+		doublesTier: "Mememons",
+	},
+};
