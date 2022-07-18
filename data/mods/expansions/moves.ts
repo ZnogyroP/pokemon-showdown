870e8826f880776404bbf116c6cdd43df2caf582
Duo <63012562+DuoM2@users.noreply.github.com>
2022-07-18 18:24:00 -0400

    Update moves.ts

--- data/mods/expansions/moves.ts
+++ data/mods/expansions/moves.ts
@@ -173,7 +173,7 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		priority: 0,
 		flags: {mystery: 1},
 		onHit(target) {
-			this.add('-start', target, 'typechange', Ghost, '[from] move: Curse of the Moon');
+			this.add('-start', target, 'typechange', 'Ghost', '[from] move: Curse of the Moon');
 			this.add('-start', target, 'typeadd', 'Dark', '[from] move: Curse of the Moon');
 		},
     boosts: {
