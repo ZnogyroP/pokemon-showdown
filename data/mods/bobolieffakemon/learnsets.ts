6bd201894e1180f0f69dc5f8e00e5ac2db56661a
Duo <63012562+DuoM2@users.noreply.github.com>
2022-08-27 15:36:03 -0400

    Create learnsets.ts

---
+++ data/mods/bobolieffakemon/learnsets.ts
@@ -0,0 +1,253 @@
+export const Learnsets: {[speciesid: string]: LearnsetData} = {
+breroot: {learnset: {
+        tackle: ["8L1"],
+        attract: ["8L1"],
+        assist: ["8L1"],
+        belch: ["8L1"],
+        bide: ["8L1"],
+        block: ["8L1"],
+        bodypress: ["8L1"],
+        bodyslam: ["8L1"],
+        bulkup: ["8L1"],
+        bulldoze: ["8L1"],
+        bulletseed: ["8L1"],
+        charge: ["8L1"],
+        chipaway: ["8L1"],
+        counter: ["8L1"],
+        dig: ["8L1"],
+        doublekick: ["8L1"],
+        doubleedge: ["8L1"],
+        drainingkiss: ["8L1"],
+        earthpower: ["8L1"],
+        endeavor: ["8L1"],
+        endure: ["8L1"],
+        extrasensory: ["8L1"],
+        facade: ["8L1"],
+        finalgambit: ["8L1"],
+        frustration: ["8L1"],
+        return: ["8L1"],
+        gigadrain: ["8L1"],
+        gigaimpact: ["8L1"],
+        grassknot: ["8L1"],
+        grassyterrain: ["8L1"],
+        growth: ["8L1"],
+        gunkshot: ["8L1"],
+        heavyslam: ["8L1"],
+        highhorsepower: ["8L1"],
+        hyperbeam: ["8L1"],
+        hiddenpower: ["8L1"],
+        hornattack: ["8L1"],
+        hornleech: ["8L1"],
+        ingrain: ["8L1"],
+        knockoff: ["8L1"],
+        lashout: ["8L1"],
+        leafblade: ["8L1"],
+        leafstorm: ["8L1"],
+        leechseed: ["8L1"],
+        leechlife: ["8L1"],
+        lick: ["8L1"],
+        lifedew: ["8L1"],
+        magicalleaf: ["8L1"],
+        magnitude: ["8L1"],
+        megahorn: ["8L1"],
+        mudshot: ["8L1"],
+        mudslap: ["8L1"],
+        naturalgift: ["8L1"],
+        naturepower: ["8L1"],
+        pollenpuff: ["8L1"],
+        protect: ["8L1"],
+        razorleaf: ["8L1"],
+        rototiller: ["8L1"],
+        rest: ["8L1"],
+        seedbomb: ["8L1"],
+        sleeptalk: ["8L1"],
+        snore: ["8L1"],
+        solarbeam: ["8L1"],
+        solarblade: ["8L1"],
+        spikes: ["8L1"],
+        spitup: ["8L1"],
+        stockpile: ["8L1"],
+        stompingtantrum: ["8L1"],
+        strength: ["8L1"],
+        substitute: ["8L1"],
+        swallow: ["8L1"],
+        swordsdance: ["8L1"],
+        synthesis: ["8L1"],
+        terrainpulse: ["8L1"],
+        thrash: ["8L1"],
+        toxic: ["8L1"],
+        wildcharge: ["8L1"],
+        grassyglide: ["8L1"],
+}},
+furnostrich: {learnset: {
+    tackle: ["8L1"],
+}},
+sholossus: {learnset: {
+    tackle: ["8L1"],
+}},
+espopod: {learnset: {
+    tackle: ["8L1"],
+}},
+croissansect: {learnset: {
+    tackle: ["8L1"],
+}},
+piedraderm: {learnset: {
+    tackle: ["8L1"],
+}},
+kakavo: {learnset: {
+    tackle: ["8L1"],
+}},
+pelli: {learnset: {
+    tackle: ["8L1"],
+}},
+dripig: {learnset: {
+    tackle: ["8L1"],
+}},
+harmoth: {learnset: {
+    tackle: ["8L1"],
+}},
+formagia: {learnset: {
+    tackle: ["8L1"],
+}},
+sodia: {learnset: {
+    tackle: ["8L1"],
+}},
+dripig: {learnset: {
+    tackle: ["8L1"],
+}},
+woosher: {learnset: {
+    tackle: ["8L1"],
+}},
+gurso: {learnset: {
+    tackle: ["8L1"],
+}},
+phelpinch: {learnset: {
+    tackle: ["8L1"],
+}},
+shiah: {learnset: {
+    tackle: ["8L1"],
+}},
+gillomen: {learnset: {
+    tackle: ["8L1"],
+}},
+furiotl: {learnset: {
+    tackle: ["8L1"],
+}},
+cuzima: {learnset: {
+    tackle: ["8L1"],
+}},
+themon: {learnset: {
+    tackle: ["8L1"],
+}},
+shocbrute: {learnset: {
+    tackle: ["8L1"],
+}},
+lightnimbus: {learnset: {
+    tackle: ["8L1"],
+}},
+cephalethal: {learnset: {
+    tackle: ["8L1"],
+}},
+malizor: {learnset: {
+    tackle: ["8L1"],
+}},
+lunheron: {learnset: {
+    tackle: ["8L1"],
+}},
+erverena: {learnset: {
+    tackle: ["8L1"],
+}},
+siltmanya: {learnset: {
+    tackle: ["8L1"],
+}},
+tudek: {learnset: {
+    tackle: ["8L1"],
+}},
+glypdorsa: {learnset: {
+    tackle: ["8L1"],
+}},
+voraciousect: {learnset: {
+    tackle: ["8L1"],
+}},
+pichiri: {learnset: {
+    tackle: ["8L1"],
+}},
+wooliba: {learnset: {
+    tackle: ["8L1"],
+}},
+olfacrid: {learnset: {
+    tackle: ["8L1"],
+}},
+rodiche: {learnset: {
+    tackle: ["8L1"],
+}},
+rodichewildwood: {learnset: {
+    tackle: ["8L1"],
+}},
+rodichehinterlands: {learnset: {
+    tackle: ["8L1"],
+}},
+rodichehive: {learnset: {
+    tackle: ["8L1"],
+}},
+mowisp: {learnset: {
+    tackle: ["8L1"],
+}},
+cephalopath: {learnset: {
+    tackle: ["8L1"],
+}},
+vambyss: {learnset: {
+    tackle: ["8L1"],
+}},
+mamicky: {learnset: {
+    tackle: ["8L1"],
+}},
+sklea: {learnset: {
+    tackle: ["8L1"],
+}},
+corazone: {learnset: {
+    tackle: ["8L1"],
+}},
+folivora: {learnset: {
+    tackle: ["8L1"],
+}},
+galena: {learnset: {
+    tackle: ["8L1"],
+}},
+grumplet: {learnset: {
+    tackle: ["8L1"],
+}},
+marlion: {learnset: {
+    tackle: ["8L1"],
+}},
+androsmos: {learnset: {
+    tackle: ["8L1"],
+}},
+dustoph: {learnset: {
+    tackle: ["8L1"],
+}},
+aeromo: {learnset: {
+    tackle: ["8L1"],
+}},
+peekoceros: {learnset: {
+    tackle: ["8L1"],
+}},
+aquimanna: {learnset: {
+    tackle: ["8L1"],
+}},
+apparicious: {learnset: {
+    tackle: ["8L1"],
+}},
+quetzadrakon: {learnset: {
+    tackle: ["8L1"],
+}},
+trybas: {learnset: {
+    tackle: ["8L1"],
+}},
+raiga: {learnset: {
+    tackle: ["8L1"],
+}},
+frogaloft: {learnset: {
+    tackle: ["8L1"],
+}},
+};
