c1dacf2f8d32837f75732f5f73995ec73030537d
Duo <63012562+DuoM2@users.noreply.github.com>
2022-11-30 10:34:20 -0500

    Update formats-data.ts

--- data/mods/gen9duomod/formats-data.ts
+++ data/mods/gen9duomod/formats-data.ts
@@ -1,134 +1,194 @@
 export const FormatsData: {[k: string]: SpeciesFormatsData} = {
     baloon: {
+		randomBattleMoves: ["playrough", "healorder", "substitute", "protect"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     fluxtapestereo: {
+		randomBattleMoves: ["flareblitz", "rapidspin", "voltswitch", "earthquake", "wildcharge", "dragondance", "bulkup", "impostorblade"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     pokat: {
+		randomBattleMoves: ["slackoff", "gigadrain", "toxic", "willowisp", "aromatherapy", "knockoff", "bodyslam", "incense"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     spirox: {
+		randomBattleMoves: ["makeitrain", "infernalparade", "nastyplot", "recover", "thunderbolt", "voltswitch", "calmmind", "trick", "spikes", "memento", "willowisp", "haze", "shadowball", "flashcannon"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     badgearth: {
+		randomBattleMoves: ["earthquake", "uturn", "dragondance", "blueshell", "firstimpression", "poisonjab", "rapidspin", "swordsdance", "shadowscratch", "impostorblade"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     bittle: {
+		randomBattleMoves: ["chillyreception", "defog", "roost", "haze", "knockoff", "stealthrock", "whirlwind", "gigatonhammer", "ironhead", "leechlife", "willowisp", "thunderwave", "toxic", "stupidcannon"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     fairydisc: {
+		randomBattleMoves: ["metronome"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     shroominesce: {
+		randomBattleMoves: ["voltswitch", "thunderbolt", "gigadrain", "agility", "moonlight", "defog", "moonblast", "energyball"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     abysseil: {
+		randomBattleMoves: ["chillyreception", "scald", "shedtail", "luminacrash", "spikes", "toxic", "lightscreen", "reflect"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     draxplosion: {
+		randomBattleMoves: ["flareblitz", "dragonhammer", "uturn", "firelash", "scaleshot", "glaiverush", "rapidspin", "defog", "onetrillionarrows", "impostorblade", "exobash"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
-    jewelode: {
-      tier: "Duomod",
+    jewelode: {/*
+		randomBattleMoves: [""],
+		randomBattleLevel: 100,
+*/      tier: "Duomod",
       doublesTier: "Duomod",
     },
     treemu: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    capsaken: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    cephalopire: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    chemiclysm: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    commanto: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    eneryth: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    falcola: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    gelsius: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    hydread: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    mountough: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    sanbatter: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    antestar: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    escarglace: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    fauxster: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    gargitect: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    temporand: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    kuribandit: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    mantelec: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
-    },
-    noxinobi: {
+		randomBattleMoves: ["leafblade", "knockoff", "bravebird", "boltbeak", "extremespeed", "roost", "impostorblade"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     impsaustor: {
+      tier: "Illegal",
+      doublesTier: "Illegal",
+    },
+    capsaken: {
+		randomBattleMoves: ["chillyreception", "ceaselessedge", "knockoff", "flareblitz", "willowisp", "toxic"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    cephalopire: {
+		randomBattleMoves: ["aquajet", "flipturn", "nuzzle", "protect", "knockoff", "rapidspin", "waterfall", "inkbrush", "polarpounce"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    chemiclysm: {
+		randomBattleMoves: ["chillyreception", "recover", "stealthrock", "sludgebomb", "flashcannon", "clearsmog", "toxicspikes", "gelatinize"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    commanto: {
+		randomBattleMoves: ["vacuumwave", "bugbuzz", "focusblast", "earthpower", "darkpulse", "fireblast", "psychic", "megahorn", "closecombat", "stoneedge", "skillswap", "stealthrock"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    eneryth: {
+		randomBattleMoves: ["voltswitch", "shadowball", "thunderbolt", "powergem", "chillyreception", "kamikaze"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    falcola: {
+		randomBattleMoves: ["defog", "sludgebomb", "roost", "whirlwind", "toxic"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gelsius: {
+		randomBattleMoves: ["scald", "fireblast", "icebeam", "willowisp", "chillyreception", "calmmind", "recover", "lavaplume", "hydropump", "substitute", "toxic"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    hydread: {
+		randomBattleMoves: ["nightshade", "willowisp", "recover", "rapidspin", "thunderwave", "toxic"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    mountough: {
+		randomBattleMoves: ["machpunch", "earthquake", "closecombat", "bulkup", "stealthrock", "poisonjab", "chillyreception", "onetrillionarrows", "impostorblade"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    sanbatter: {
+		randomBattleMoves: ["poltergeist", "closecombat", "shadowsneak", "machpunch", "drainpunch", "swordsdance", "bulkup", "bellydrum", "shadowscratch"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    antestar: {
+		randomBattleMoves: ["uturn", "ironhead", "playrough", "bulletpunch", "exobash", "zingzap", "trick"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    escarglace: {
+		randomBattleMoves: ["icebeam", "earthpower", "nastyplot", "chillyreception", "freezedry", "sludgebomb"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    fauxster: {
+		randomBattleMoves: ["wavecrash", "closecombat", "swordsdance", "liquidation", "drainpunch", "earthquake", "knockoff", "jetpunch"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    gargitect: {
+		randomBattleMoves: ["stoneedge", "dragonhammer", "knockoff", "swordsdance", "stoneaxe", "accelerock", "flareblitz"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    temporand: {
+		randomBattleMoves: ["scorchingsands", "calmmind", "recover", "stealthrock", "haze", "psychic", "hyperwind", "earthpower", "pharaohshot", "hyperwind", "dazzlinggleam"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    kuribandit: {
+		randomBattleMoves: ["hyperwind", "darkpulse", "nastyplot", "agility", "calmmind"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    mantelec: {
+		randomBattleMoves: ["zingzap", "earthquake", "lunge", "exobash", "swordsdance", "uturn", "shadowscratch"],
+		randomBattleLevel: 100,
+      tier: "Duomod",
+      doublesTier: "Duomod",
+    },
+    noxinobi: {
+		randomBattleMoves: ["direclaw", "wildcharge", "zenheadbutt", "bulkup", "waterfall", "aquajet", "impostorblade", "onetrillionarrows", "shadowscratch", "inkbrush"],
+		randomBattleLevel: 100,
       tier: "Duomod",
       doublesTier: "Duomod",
     },
     gelsiussubzero: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
+      tier: "Illegal",
+      doublesTier: "Illegal",
     },
     gelsiushundred: {
-      tier: "Duomod",
-      doublesTier: "Duomod",
+      tier: "Illegal",
+      doublesTier: "Illegal",
     },
 };
